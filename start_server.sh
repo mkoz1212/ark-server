7334a43814d7750ebc5af7243fe63dc36a18516c
Michael Kozielski <michael.kozielski@googlemail.com>
2021-02-26 17:20:00 +0100

    improved startup script

--- start_server.sh
+++ start_server.sh
@@ -15,37 +15,35 @@ function start_server {
     #GAME_OPTS="${GAME_OPTS}?ServerPassword=bert1234"
     #GAME_OPTS="${GAME_OPTS}?ServerAdminPassword=damian1705"
     GAME_OPTS="${GAME_OPTS}?MaxPlayers=8"
-    GAME_OPTS="${GAME_OPTS}?ClampItemStats=false"
-    GAME_OPTS="${GAME_OPTS}?ClampItemSpoilingTimes=false"
     GAME_OPTS="${GAME_OPTS}?Port=${PORT}"
     GAME_OPTS="${GAME_OPTS}?QueryPort=${QUERY_PORT}"
     GAME_OPTS="${GAME_OPTS}?DisableWeatherFog=true"
+
+    GAME_PARAMS="-clusterid=${CLUSTER}"
+    GAME_PARAMS="${GAME_PARAMS} -NoBattleEye"
+    GAME_PARAMS="${GAME_PARAMS} -noantispeedhack"
+    GAME_PARAMS="${GAME_PARAMS} -exclusivejoin"
+    GAME_PARAMS="${GAME_PARAMS} -ForceAllowCaveFlyers"
+#    GAME_PARAMS="${GAME_PARAMS} -ActiveEvent=vday"
+    GAME_PARAMS="${GAME_PARAMS} -server -high -log"
+
     echo "Starting ARK Server with map: $MAP"
     echo "Using port: $PORT"
     echo "Using query port: $QUERY_PORT"
     echo "GAME_OPTS:"
     echo $GAME_OPTS
+    echo "GAME_PARAMS:"
+    echo $GAME_PARAMS
 
     docker run -d --rm \
         --volume ${HOST_ARK_PATH}:${GUEST_ARK_PATH} \
-	--name ArkServer-${MAP} \
-	--network host \
-	--ulimit nofile=1000000:1000000 \
-	--workdir="/ark/ShooterGame/Saved/Logs" \
-	ark-server:latest \
-	/ark/ShooterGame/Binaries/Linux/ShooterGameServer ${GAME_OPTS} \
-	    -clusterid=${CLUSTER} \
-	    -NoBattlEye \
-	    -noantispeedhack \
-	    -ForceAllowCaveFlyers \
-	    -exclusivejoin \
-	    -server \
-	    -log 
+        --name ArkServer-${MAP} \
+        --network host \
+        --ulimit nofile=1000000:1000000 \
+        --workdir="/ark/ShooterGame/Saved/Logs" \
+        ark-server:latest \
+        /ark/ShooterGame/Binaries/Linux/ShooterGameServer ${GAME_OPTS} ${GAME_PARAMS}
 }
 
 start_server $MAP
 
-
-#	-p ${PORT}:${PORT}/udp \
-#	-p $((PORT+1)):$((PORT+1))/udp \
-#	-p ${QUERY_PORT}:${QUERY_PORT}/udp \
