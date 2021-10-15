4d89b39fe217565f7bada1bc377bed01cc18fcd6
Michael Kozielski <michael.kozielski@googlemail.com>
2021-10-15 22:22:47 +0200

    Update some server flags

--- start_server.sh
+++ start_server.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 
-source config.sh
-
 MAP=${1:-TheIsland}
-PORT=${2:-7777}
-QUERY_PORT=$((27015+PORT-7777))
 CLUSTER=mickoz84_cluster
+
+source config.sh
+PORT=${2:-$PORT}
+QUERY_PORT=$((27015+PORT-7777))
+
 
 function start_server {
     MAP=$1
     GAME_OPTS="${MAP}?listen"
     GAME_OPTS="${GAME_OPTS}?SessionName=${CLUSTER}_${MAP}"
     GAME_OPTS="${GAME_OPTS}?AltSaveDirectoryName=${CLUSTER}_${MAP}"
-    #GAME_OPTS="${GAME_OPTS}?ServerPassword=bert1234"
-    #GAME_OPTS="${GAME_OPTS}?ServerAdminPassword=damian1705"
+    #GAME_OPTS="${GAME_OPTS}?ServerPassword="
+    #GAME_OPTS="${GAME_OPTS}?ServerAdminPassword="
     GAME_OPTS="${GAME_OPTS}?MaxPlayers=8"
     GAME_OPTS="${GAME_OPTS}?Port=${PORT}"
     GAME_OPTS="${GAME_OPTS}?QueryPort=${QUERY_PORT}"
     GAME_OPTS="${GAME_OPTS}?DisableWeatherFog=true"
 
     GAME_PARAMS="-clusterid=${CLUSTER}"
-    GAME_PARAMS="${GAME_PARAMS} -NoBattleEye"
+    GAME_PARAMS="${GAME_PARAMS} -NoBattlEye"
     GAME_PARAMS="${GAME_PARAMS} -noantispeedhack"
     GAME_PARAMS="${GAME_PARAMS} -exclusivejoin"
     GAME_PARAMS="${GAME_PARAMS} -ForceAllowCaveFlyers"
+    GAME_PARAMS="${GAME_PARAMS} -NoTransferFromFiltering"
 #    GAME_PARAMS="${GAME_PARAMS} -ActiveEvent=vday"
+#    GAME_PARAMS="${GAME_PARAMS} -ActiveEvent=Easter"
+#    GAME_PARAMS="${GAME_PARAMS} -ActiveEvent=Summer"
     GAME_PARAMS="${GAME_PARAMS} -server -high -log"
 
     echo "Starting ARK Server with map: $MAP"
