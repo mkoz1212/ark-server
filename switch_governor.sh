d2c4b42444a7c0fbc39e92e93578e34e816cb252
Michael Kozielski <michael.kozielski@googlemail.com>
2021-10-15 22:26:12 +0200

    cleanup

--- switch_governor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-GOV=$1
-
-for cpu in $(ls -vd /sys/devices/system/cpu/cpu*/cpufreq/); do
-    echo "$GOV" > $cpu/scaling_governor
-done
-
-cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
