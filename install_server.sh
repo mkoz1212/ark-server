194e2e3639392c4f8708255e426e95f1d4f003ee
Michael Kozielski <michael.kozielski@googlemail.com>
2021-10-15 22:19:25 +0200

    use latest local image to install server

--- install_server.sh
+++ install_server.sh
@@ -5,5 +5,5 @@ source config.sh
 docker run -it --rm \
     --volume ${HOST_ARK_PATH}:${GUEST_ARK_PATH} \
     --name ArkServer-Setup \
-    ark-server \
+    ark-server:latest \
     /bin/bash -c "/home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /ark +app_update 376030 +quit"
